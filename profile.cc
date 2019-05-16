a3b8c4fe3fad5f5300398b96703f29929b876041
ElspethBair <jellybelly1713@gmail.com>
2019-05-15 20:43:39 -0700

    Complete 1st task

--- profile.cc
+++ profile.cc
@@ -1,5 +1,9 @@
 #include "profile.h"
+#include <cmath>
+#include <iostream>
 
 const double Profile::GetTime() {
-  return 0.0;
+  double total_time;
+  total_time = ((std::sqrt(2))/kMaxAcceleration);
+  return total_time;
 }
--- profile.h
+++ profile.h
@@ -1,8 +1,8 @@
 #ifndef PROFILE_H_
 #define PROFILE_H_
 
-constexpr kMaxVelocity = 1.0;
-constexpr kMaxAcceleration = 1.0;
+constexpr double kMaxVelocity = 1.0;
+constexpr double kMaxAcceleration = 1.0;
 
 class Profile {
 public:
