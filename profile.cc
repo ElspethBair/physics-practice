48df9de7caa9fc9579726ac53db4f4b4f5ebe471
ElspethBair <jellybelly1713@gmail.com>
2019-05-20 21:44:54 -0700

    Finish second part of problem

--- profile.cc
+++ profile.cc
@@ -2,7 +2,22 @@
 #include <cmath>
 
 const double Profile::GetTime() {
+  bool max_v = true;
   double total_time;
-  total_time = ((std::sqrt(2))/kMaxAcceleration);
-  return total_time;
+  double t1;
+  double d1;
+  double d2; 
+
+  if (max_v == false){ 
+    total_time = ((std::sqrt(2))/kMaxAcceleration);
+    return total_time;
+  }
+  else {
+    t1 = (kMaxVelocity/kMaxAcceleration);
+    d1 = (0.5 * kMaxVelocity * t1);
+    d2 = 1 - (2 * d1);
+    total_time = (2*(t1/kMaxAcceleration)) + (d2/kMaxVelocity); 
+    return total_time;
+    
+  }
 }
